ba0001a2f3234005de57f672d4fd4d70a4b315b8
JinHua Lin <jinhua.lin@utb.tyreso.se>
2025-09-06 20:59:30 +0200

    Add files via upload

---
+++ Models/Calculation.cs
@@ -0,0 +1,13 @@
+﻿namespace AdditionApi
+{
+
+    public class Calculation
+    {
+        public double QT { get; set; }
+        public double VOL { get; set; }
+
+        public string TMP { get; set; } = string.Empty;
+
+        public double Result { get; set; }
+    }
+}
