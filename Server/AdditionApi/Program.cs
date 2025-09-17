e3d3d582fbcacb06b3aa62aa4a8fd619bfcbadae
LaRyTyreso <lana.ryzhova@utb.tyreso.se>
2025-09-17 16:37:13 +0200

    feat: start MSSQL in Docker for Development via DockerStarter; load .env; add migrations and solution

--- Server/AdditionApi/Program.cs
+++ Server/AdditionApi/Program.cs
@@ -1,21 +1,46 @@
 using AdditionApi;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using DotNetEnv;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// add DbContext and connection string from appsettings.json
-builder.Services.AddDbContext<AppDbContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+// Загружаем .env
+Env.Load();
 
-// Adding controllers
+// Читаем переменные окружения
+var saPassword = Environment.GetEnvironmentVariable("SA_PASSWORD") ?? "Your_password123!";
+var dbName = Environment.GetEnvironmentVariable("MSSQL_DB") ?? "MyAppDb";
+var host = Environment.GetEnvironmentVariable("MSSQL_HOST") ?? "127.0.0.1";
+var port = Environment.GetEnvironmentVariable("MSSQL_PORT") ?? "1433";
+
+// Формируем строку подключения вручную
+var connectionString =
+    $"Server={host},{port};Database={dbName};User Id=sa;Password={saPassword};TrustServerCertificate=True;";
+
+// Записываем строку подключения в конфигурацию
+builder.Configuration["ConnectionStrings:DefaultConnection"] = connectionString;
+
+// Подключаем сервисы
 builder.Services.AddControllers();
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlServer(connectionString));
 
 var app = builder.Build();
 
+if (app.Environment.IsDevelopment())
+{
+    Console.WriteLine(">>> ENV is Development, starting Docker container...");
+    await DockerStarter.EnsureSqlContainerAsync(app.Configuration);
+}
+
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+    db.Database.Migrate();
+}
+
 app.UseHttpsRedirection();
 app.UseAuthorization();
-
 app.MapControllers();
 
 app.Run();
